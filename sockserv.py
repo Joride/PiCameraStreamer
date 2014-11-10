5ad2e0d8d82f843445cfd6e8e87d62927880367e
Joride <kerrelinc@gmail.com>
2014-11-10 19:18:33 +0000

    reading of binary bytes succeeds and turning camera on or off in response too

--- sockserv.py
+++ sockserv.py
@@ -3,12 +3,6 @@ import socket               # Import socket module
 import picamera
 import time
 
-
-camera = picamera.PiCamera()
-camera.framerate = 20
-print 'resolution is', camera.resolution
-print "framerate is ", camera.framerate
-
 print "setting up socket..."
 socket = socket.socket()
 # host = "192.168.2.1"
@@ -19,26 +13,56 @@ socket.bind((host, port))
 socket.listen(5)
 print "socket ready %s : %s" % (host, port) 
 
-while True:
-    connection, addr = socket.accept()
-    print 'Got connection from', addr
-    # connection.send('Thank you for connecting')
-    
+def startRecordingIntoStream(stream):
     print "now starting camera recording"
-    fileLikeObject = connection.makefile('rb')
-
+    camera = picamera.PiCamera()
+    camera.framerate = 20
+    print 'resolution is', camera.resolution
+    print "framerate is ", camera.framerate
     camera.brightness = 80
-#    camera.start_recording(fileLikeObject, format='h264', intra_period = 1, inline_headers = False)
+
+    fileLikeObject = connection.makefile('rb')
     camera.start_recording(fileLikeObject, format='h264', intra_period = 1, bitrate=2500000)
-    # camera.wait_recording(300)
-    #print "now stopping camera recording"
-    # camera.stop_recording()
+    # camera.wait_recording(600)
+    print "camera is now recording"
 
-    # The constants SHUT_RD, SHUT_WR, SHUT_RDWR have the values 0, 1, 2,
-    # respectively, and are defined in <sys/socket.h> since glibc-2.1.91.
-    #print "now closing the connection"
-    #connection.shutdown(2)
-    #connection.close()            
+    return camera
 
-socket.shutdown(2)
-socket.close
+def stopRecordingIntoStream(stream, camera):
+    print "now stopping camera recording"
+    camera.stop_recording()
+
+while True:
+    try:
+        connection, addr = socket.accept()
+        print 'Got connection from %s %s' % (connection, addr)
+
+        length = None
+        buffer = ""
+        while True:
+            data = connection.recv(1024)
+            
+            
+            for byte in data:
+                print "byte is: ", ord(byte)
+                if ord(byte) == 1<<7:
+                    print "recording data"
+                    camera = startRecordingIntoStream(connection)
+                elif ord(byte) == 1<<6:
+                     print "stopping recording"
+                     stopRecordingIntoStream(connection, camera)
+
+
+    except ValueError:
+        print "ERROR: ", ValueError
+        socket.shutdown(2)
+        socket.close
+        break
+
+
+# The constants SHUT_RD, SHUT_WR, SHUT_RDWR have the values 0, 1, 2,
+# respectively, and are defined in <sys/socket.h> since glibc-2.1.91.
+# print "now closing the connection"
+# connection.shutdown(2)
+# connection.close()            
+
