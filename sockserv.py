57b749f48cdff1b567bbb066a95409efb188a449
Joride <kerrelinc@gmail.com>
2014-11-11 20:07:16 +0000

    firt steps onto implementing a remote control protcol

--- sockserv.py
+++ sockserv.py
@@ -3,6 +3,8 @@ import socket               # Import socket module
 import picamera
 import time
 
+camera = picamera.PiCamera()
+
 print "setting up socket..."
 socket = socket.socket()
 # host = "192.168.2.1"
@@ -13,9 +15,11 @@ socket.bind((host, port))
 socket.listen(5)
 print "socket ready %s : %s" % (host, port) 
 
+kBeginOfMessage  = 0b10000000
+kEndOfMessage    = 0b11000000
+
 def startRecordingIntoStream(stream):
     print "now starting camera recording"
-    camera = picamera.PiCamera()
     camera.framerate = 20
     print 'resolution is', camera.resolution
     print "framerate is ", camera.framerate
@@ -23,42 +27,52 @@ def startRecordingIntoStream(stream):
 
     fileLikeObject = connection.makefile('rb')
     camera.start_recording(fileLikeObject, format='h264', intra_period = 1, bitrate=2500000)
-    # camera.wait_recording(600)
     print "camera is now recording"
-
-    return camera
 
 def stopRecordingIntoStream(stream, camera):
     print "now stopping camera recording"
     camera.stop_recording()
+
+def handleMessage(message):
+    for byte in message:
+        print "byte: ", ord(byte)
+
+    print "-- end of message --"
+    if ord(message[1]) == 0b00000001:
+        startRecordingIntoStream(connection)
+
+    if ord(message[1]) == 0b00000000:
+        stopRecordingIntoStream(connection, camera)
+
 
 while True:
     try:
         connection, addr = socket.accept()
         print 'Got connection from %s %s' % (connection, addr)
 
-        length = None
-        buffer = ""
+        currentMessage = None
         while True:
             data = connection.recv(1024)
             
-            
             for byte in data:
-                print "byte is: ", ord(byte)
-                if ord(byte) == 1<<7:
-                    print "recording data"
-                    camera = startRecordingIntoStream(connection)
-                elif ord(byte) == 1<<6:
-                     print "stopping recording"
-                     stopRecordingIntoStream(connection, camera)
+                binaryByte  = ord(byte)
+                if binaryByte == kBeginOfMessage:
+                    currentMessage = byte
 
+                elif binaryByte == kEndOfMessage:
+                    currentMessage += byte
+                    handleMessage(currentMessage)
+                    currentMessage = None
+
+                else:
+                    currentMessage += byte
+                     
 
     except ValueError:
         print "ERROR: ", ValueError
-        socket.shutdown(2)
+        socket.shutdown(2)  
         socket.close
         break
-
 
 # The constants SHUT_RD, SHUT_WR, SHUT_RDWR have the values 0, 1, 2,
 # respectively, and are defined in <sys/socket.h> since glibc-2.1.91.
