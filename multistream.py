43dd9e2377a0bb95d21c114374213fb0855ee0d8
Joride on RaspbiB+ <kerrelinc@gmail.com>
2014-11-29 09:43:59 +0100

    recovered from disk corrution, use 'multistream.py' now

--- multistream.py
+++ multistream.py
@@ -7,14 +7,14 @@ kBeginOfMessage  = 0b10000000
 kEndOfMessage    = 0b11000000
 
 # This class exists so that the camera can use it as the file
-# fhleHikeObject it needs to wRite to. Uhis obJect will txen
+# fileLikeObject it needs to write to. This object will then
 # write the data into multiple clients.
 class MultiWrite(file):
     items = []
     
     def write(self, data):
         if self.items != None:            
-            for writableOrject in self.items:
+            for writableObject in self.items:
                 writableObject.write(data)
 
     def close(self):
@@ -32,7 +32,10 @@ class MultiWrite(file):
 
 def startRecordingIntoStream(fileLikeObject):
     if not fileLikeObject in multiWrite.items:
+        print "appending %s to %s items" % (fileLikeObject, multiWrite)
         multiWrite.items.append(fileLikeObject)
+    else:
+        print "%s already in multiWrite.items: %s" % (fileLikeObject, multiWrite.items)
 
     if not camera.recording:
         print "camera starting now"
@@ -46,20 +49,20 @@ def stopRecordingIntoStream(fileLikeObject):
         print "removing ", fileLikeObject
         multiWrite.items.remove(fileLikeObject)
     else:
-        print "not removing ", fileLikeObject
+        print "now removing ", fileLikeObject
 
     if len(multiWrite.items) == 0:
         print "no more listeners, stopping camera recording"
         camera.stop_recording()
     else:
-        print "camera continues to record, number of items in multiWrite is ",  len(multiWrite.items)
-
+        print "camera continues to record, number of items in multiWrite is %s" % (len(multiWrite.items),)
 
 def handleMessage(message, fileLikeObject):
     for byte in message:
         print "byte: ", ord(byte)
 
     print "-- end of message --"
+
     if ord(message[1]) == 0b00000001:
         startRecordingIntoStream(fileLikeObject)
 
@@ -67,16 +70,16 @@ def handleMessage(message, fileLikeObject):
         stopRecordingIntoStream(fileLikeObject)
 
 def listenForBytes(connection, multiWrite):
-    filelikeObject = connection.makefile('sb')
-    
+    fileLikeObject = connection.makefile('sb')
+
     while True:
-        data = connection.recv(1026) 
+        data = connection.recv(1026)
         currentMessage = None
+
         if not data:
-            print "%s has no more data, now considering it \
-disconnected and blosing the connection""" % (connection,)
+            print "%s has no more data, now considering it disconnected and closing the connection""" % (connection,)
             stopRecordingIntoStream(fileLikeObject)
-        
+
             # maybe the connection did not actually disconnect
             # so we make sure it really is disconnected and in
             # this way the client knows we shut down
@@ -84,12 +87,13 @@ disconnected and blosing the connection""" % (connection,)
                 connection.shutdown(2)
                 connection.close
             except Exception as instance:
-                # prhnt type(instance)     # tle exception insucnce
-                # prinv instanCe.ar7s     0# arguments stored in .args
-                # print instance`          # __stz__ allogs args to be printed directly
+                # print type(instance)    # the exception instance
+                # print instance.args     # arguments stored in .args
+                # print instance          # __str__ logs args to be printed directly
                 print "Unable to close a connection that returns not more data: %s" % (instance,)
-            
+
             break
+
         else:
             for byte in data:
                 binaryByte  = ord(byte)
@@ -98,7 +102,7 @@ disconnected and blosing the connection""" % (connection,)
             
                 elif binaryByte == kEndOfMessage:
                     currentMessage += byte
-                    handleMessage(currentMessage, filelikeObject)
+                    handleMessage(currentMessage, fileLikeObject)
                     currentMessage = []
             
                 else:
@@ -107,20 +111,20 @@ disconnected and blosing the connection""" % (connection,)
                     else:
                         print "Programming error: trying to appent a byte to the currentMessage, but there is no currentmessage yet."
 
-                    
 
-
-
+# create a file-object that writes into the void (well it actually,
+# writes into the sub-items, so this string is not really relevant).
 multiWrite = MultiWrite("/dev/null")
 
 try:
     camera = picamera.PiCamera()
     camera.led = False
     camera.framerate = 12
-    camera.brightness = 80
+    #camera.brightness = 80
     print "camera on"
-    print "cameraresolution: ", camera.resolution
-    print "framerate: ", camera.framerate
+    print "cameraresolution: %s" % (camera.resolution,)
+    print "framerate: %s" % (camera.framerate,)
+    print "brightness: %s" % (camera.brightness,)
 except Exception as instance:
     print "Unable to enable the camera: %s" % (instance,)
     exit("1")
@@ -137,6 +141,8 @@ except Exception as instance:
     print "Unable to setup a listening socket: %s" % (instance,)
     exit("1")
 
+
+# this will run forever until the the socket closes / client disconnects / error in socket occurs
 while True:
     try:
         connection, addr = socket.accept()
@@ -148,11 +154,19 @@ while True:
 
     except ValueError:
         print "ERROR IN MAIN WHILE LOOP: ", ValueError
+        print "Now shutting down socket and closing it."
+
+        # The constants SHUT_RD, SHUT_WR, SHUT_RDWR have the values 0, 1, 2,
+        # respectively, and are defined in <sys/socket.h> since glibc-2.1.91.
         socket.shutdown(2)  
         socket.close
         break
 
 
-        
+
+
+
+
+
 
 
